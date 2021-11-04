f27081c0c3302d391a932e575f4dfeb1e3f09094
RetaHo <reta.ho@getbrightauto.com>
2021-11-04 10:19:53 +0800

    form test

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,17 @@
   <div>Hello Word!</div>
   <div>Hello Word!</div>
   <div>Reta 你好！</div>
+  <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
   <router-view/>
 </template>
 
